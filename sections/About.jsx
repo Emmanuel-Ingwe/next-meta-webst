8082711ff1703e1cafff437de18ddc402a90b122
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-03-07 00:36:58 +0100

    __

--- sections/About.jsx
+++ sections/About.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 import { TypingText } from "../components";
 
 import styles from "../styles";
-import { fadeIn, staggerContainer } from "../utils/mot ion";
+import { fadeIn, staggerContainer } from "../utils/motion";
 
 const About = () => (
 	<section className={`${styles.paddings} relative z-10`}>

9ee0a501ec53c633c43d70be6da1009cac322877
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-06-29 15:39:57 +0100

    _appp

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,9 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
+  "extends": [
+    "next",
+    "plugin:next/recommended"
   ],
   parserOptions: {
     ecmaFeatures: {
